8cc108fb820e1c2c942887139897755f062cfe4d
Natalia Camarinha <natalia.camarinha@pill.com.br>
2022-08-15 20:30:52 -0300

    fix: postgres connection

--- src/infra/database/PgPromiseConnection.ts
+++ src/infra/database/PgPromiseConnection.ts
@@ -5,7 +5,7 @@ export default class PgPromiseConnection implements Connection {
   connection: any;
 
   constructor() {
-    this.connection = pgp()("postgres://postgres:123@localhost:5432");
+    this.connection = pgp()("postgres://postgres:postgres@localhost:5432");
   }
   query(statement: string, params: any): Promise<any> {
     return this.connection.query(statement, params);
